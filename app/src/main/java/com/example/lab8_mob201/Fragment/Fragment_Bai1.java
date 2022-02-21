f1185c9e043429001891fa3d06bdaabe483c76ae
tuvvph09410 <tucaogia1@gmail.com>
2022-02-21 08:51:13 +0700

    upload lab8 lần 2 tuvvph09410

--- app/src/main/java/com/example/lab8_mob201/Fragment/Fragment_Bai1.java
+++ app/src/main/java/com/example/lab8_mob201/Fragment/Fragment_Bai1.java
@@ -4,14 +4,8 @@ import static android.app.Activity.RESULT_OK;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,25 +17,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.lab8_mob201.R;
-import com.example.lab8_mob201.ReSources.AlbumStorageDirFactory;
-import com.example.lab8_mob201.ReSources.BaseAlbumDirFactory;
-import com.example.lab8_mob201.ReSources.FroyoAlbumDirFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class Fragment_Bai1 extends Fragment {
     private Button btnCamera;
     private ImageView ivCamera;
-    private final int REQUEST_IMAGE_CAPTURE = 1;
-    private final int REQUEST_TAKE_PHOTO = 1;
-    private static final String JPEG_FILE_PREFIX = "IMG_";
-    private static final String JPEG_FILE_SUFFIX = ".jpg";
-    private AlbumStorageDirFactory albumStorageDirFactory = null;
-    private String mCurrentPhotoPath;
+    private static final int CAMERA_ACTION_CODE = 1;
 
 
     @Override
@@ -64,14 +45,15 @@ public class Fragment_Bai1 extends Fragment {
     }
 
     private void accessCamera() {
-        int numberCamera = Camera.getNumberOfCameras();
-        if (numberCamera > 0) {
-            Toast.makeText(getContext(), "Thiet bi co" + numberCamera, Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, 1);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_ACTION_CODE);
         } else {
-            Toast.makeText(getContext(), "Thiet bi khong co camera", Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "Không có thiết bị hộ trợ", Toast.LENGTH_SHORT).show();
         }
+
+
     }
 
     private void initByViewId(View view) {
@@ -79,4 +61,13 @@ public class Fragment_Bai1 extends Fragment {
         this.ivCamera = view.findViewById(R.id.iv_camera);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            this.ivCamera.setImageBitmap(bitmap);
+        }
+    }
 }
